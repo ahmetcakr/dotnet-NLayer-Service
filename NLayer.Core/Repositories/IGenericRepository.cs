5da0eacc15358f398ad433a41803536e82491c23
Cakir <ahmetcaakirr@gmail.com>
2022-08-20 21:56:54 +0300

    IGenericRepository / IService / IUnıtOfWorks

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Repositories
+{ // Veritabanına yapılacak temel sorgular burada oluşturulur.
+    public interface IGenericRepository<T> where T : class 
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        // Daha fazla performans için / sorgular direkt veritabanına gitmez / order by gibi işlemler için kullanılır
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task<T> AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();  // Bilerek farklı yapıldı
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task<T> AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        // Değişiklikleri veritabanına yansıtacağımız için burada void kullanmıyoruz
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
